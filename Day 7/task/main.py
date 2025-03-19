c9f08a4d7ee3db94f9d40492932b9ec72f0ac390
ridoy2000 <ridoy.asad3000@gmail.com>
2025-03-19 00:07:45 +0000

    Day 7 exercises

--- Day 7/task/main.py
+++ Day 7/task/main.py
@@ -1,8 +1,39 @@
+import random
+from sys import displayhook
+
 word_list = ["aardvark", "baboon", "camel"]
 
-# TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word. Then print it.
+chosen_word = random.choice(word_list)
+print(chosen_word)
 
-# TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+placeholder = ""
+word_length = len(chosen_word)
+for position in range(word_length):
+    placeholder += "_"
+print(placeholder)
 
-# TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word. Print "Right" if it
-#  is, "Wrong" if it's not.
+# TODO-1: - Use a while loop to let the user guess again.
+game_over = False
+correct_letters = []
+
+while not game_over:
+    guess = input("Guess a letter: ").lower()
+
+    display = ""
+
+    # TODO-2: Change the for loop so that you keep the previous correct letters in display.
+
+    for letter in chosen_word:
+        if letter == guess:
+            display += letter
+            correct_letters.append(guess)
+        elif letter in correct_letters:
+            display += letter
+        else:
+            display += "_"
+
+    print(display)
+
+    if "_" not in display:
+        game_over = True
+        print("you win")
